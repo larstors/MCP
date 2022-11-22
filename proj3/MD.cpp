10d2b9983b5d697e786da68a1d8a7326b0ef5fc3
vetkje <larsgallardo@hotmail.no>
2022-11-22 19:18:18 +0100

    fixed potential

--- proj3/MD.cpp
+++ proj3/MD.cpp
@@ -19,25 +19,27 @@
 
 
 using namespace std;
-using vecd = std::vector<double>;
+using vecd = std::vector<long double>;
 using vec = std::vector<int>;
 
+template<typename Engine> class ClusterWriter;
 
 
 struct Parameters {
     double      mass = 1.0;     // assuming the particles have the same mass
     vec         L {10, 20, 30}; // dimensions of box
     double      T0 = 1;         // initial temperature
-    unsigned    N = 100;        // number of particles
+    int    N = 100;        // number of particles
 };
 
 template<typename Engine>
 class MD {
 
+    friend class ClusterWriter<Engine>;
+
     struct Particle{
         vecd position = vecd(3);    // position of partilce
-        vecd momentum = vecd(3);    // momentum of particle
-
+        vecd velocity = vecd(3);    // velocity of particle
     };
 
 
@@ -58,8 +60,34 @@ class MD {
         // vector with force for each particle and each component
         vecd f(P.N * 3);
 
+        vecd dist = minimal_distance();
 
-
+        // loop over output for particles
+        for (int i = 0; i < P.N; i++){
+            // loop over all particles
+            for (int j = 0; j < P.N; j++){
+                if (j > i){
+                    // only want difference between different particles
+                    vecd new_r (3);
+                    for (int k = 0; k < 3; k++){
+                        if (fabs(future[i].position[k] - future[j].position[k]) < double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k];
+                        }
+                        else if (future[i].position[k] - future[j].position[k] > double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k] - P.L[0];
+                        }
+                        else if (future[i].position[k] - future[j].position[k] < double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k] + P.L[0];
+                        }
+                        if (dist[P.N*i + j] > 1e-5 && dist[P.N*i + j] < 2.5){
+                            f[3*i + k] += (1 / pow(dist[P.N*i + j], 14) - 0.5 / pow(dist[P.N*i + j], 8)) * (future[i].position[k] - new_r[k]);
+                            f[3*j + k] -= (1 / pow(dist[P.N*i + j], 14) - 0.5 / pow(dist[P.N*i + j], 8)) * (future[i].position[k] - new_r[k]);
+                        }
+                    }
+                    
+                }
+            }
+        }
         return f;
     }
 
@@ -71,30 +99,82 @@ class MD {
      * @param n 
      * @return double 
      */
-    double distance(vecd p1, vecd p2, vec n){
+    double distance(vecd p1, vecd p2){
         // distance variable
         double dist = 0;
 
         for (int i = 0; i < 3; i++){
-            dist = pow(p1[i] - p2[i] - n[i] * P.L[i], 2);
+            dist += (p1[i] - p2[i]) * (p1[i] - p2[i]);
         }
 
-        return dist;
+        return pow(dist, 1.0/2.0);
     }
 
     vecd minimal_distance(){
-        vecd output;
+        vecd output (P.N * P.N);
 
         // loop over output for particles
         for (int i = 0; i < P.N; i++){
             // loop over all particles
             for (int j = 0; j < P.N; j++){
-                // only want difference between different particles
-                if (i != j){
-                    
+                if (j > i){
+                    // only want difference between different particles
+                    vecd new_r (3);
+                    for (int k = 0; k < 3; k++){
+                        if (fabs(future[i].position[k] - future[j].position[k]) < double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k];
+                        }
+                        else if (future[i].position[k] - future[j].position[k] > double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k] + P.L[0];
+                        }
+                        else if (future[i].position[k] - future[j].position[k] < - double(P.L[0]) / 2){
+                            new_r[k] = future[j].position[k] - P.L[0];
+                        }
+                    }
+                    if (distance(future[i].position, new_r) < 1e-5){
+                         cout << "WTF " << i << " " << j << " " << distance(future[i].position, new_r) << endl;
+                         for (int m = 0; m < 3; m++){
+                            cout << future[i].position[m] << " " << future[j].position[m]  << " "<< new_r[m] << endl;
+                         }
+                    }
+                    output[P.N * i + j] = distance(future[i].position, new_r);
+                }
+                else {
+                    output[P.N * i + j] = 0;
                 }
             }
         }
+        return output;
+    }
+
+    double lenJones(double r){
+        return 4 * (1/pow(r, 12) - 1/pow(r, 6));
+    }
+
+    vecd potential(vecd r){
+        vecd output (P.N * P.N);
+
+        // loop over output for particles
+        for (int i = 0; i < P.N; i++){
+            // loop over all particles
+            for (int j = 0; j < P.N; j++){
+                if (r[P.N * i + j] > 2.5){
+                    output[P.N * i + j] = 0;
+                    }
+                else {
+                    if (j > i){
+                        output[P.N * i + j] = lenJones(r[P.N * i + j]);
+                    }
+                    else if (i == j){
+                        output[P.N * i + j] = 0;
+                    }
+                    else {
+                        output[P.N * i + j] = lenJones(r[P.N * j + i]);
+                    }
+                }
+            }
+        }
+        return output;
     }
 
 
@@ -114,14 +194,14 @@ class MD {
             int M3 = P.N/4;
             double M = pow(M3, 1.0/3.0);
             // to achieve p_tot = 0 we need a vector for the mean
-            vecd mean_momentum {0, 0, 0};
-            // initialise the position and momentum of each particle drawn from random distributions
-            for (unsigned i = 0; i < P.N; i++){
-                for (unsigned k = 0; k < 3; k++){
-                    // draw momentum from normal distribution, respectively
-                    past[i].momentum[k] = maxwell(rng);
-                    // add the momentum to the total, i.e. mean, momentum
-                    mean_momentum[k] += past[i].momentum[k];
+            vecd mean_velocity {0, 0, 0};
+            // initialise the position and velocity of each particle drawn from random distributions
+            for (int i = 0; i < P.N; i++){
+                for (int k = 0; k < 3; k++){
+                    // draw velocity from normal distribution, respectively
+                    past[i].velocity[k] = maxwell(rng);
+                    // add the velocity to the total, i.e. mean, velocity
+                    mean_velocity[k] += past[i].velocity[k];
                 }
             }
 
@@ -155,24 +235,24 @@ class MD {
                 }
             }
 
-            // normalise the mean momentum so that the sum goes to zero
+            // normalise the mean velocity so that the sum goes to zero
             for (int i = 0; i < 3; i++){
-                mean_momentum[i] /= double(P.N);
+                mean_velocity[i] /= double(P.N);
             }
 
-            // remove the mean from each particle so that the total momentum is 0
-            for (unsigned i = 0; i < P.N; i++){
-                for (unsigned k = 0; k < 3; k++){
-                    past[i].momentum[k] -= mean_momentum[k];
+            // remove the mean from each particle so that the total velocity is 0
+            for (int i = 0; i < P.N; i++){
+                for (int k = 0; k < 3; k++){
+                    past[i].velocity[k] -= mean_velocity[k];
                 }
             }
             
             // just testing some stuff
             ofstream outfile;
             outfile.open("test.txt");
-            for (unsigned i = 0; i < P.N; i++){
-                for (unsigned k = 0; k < 3; k++){
-                    outfile << past[i].position[k] << " ";
+            for (int i = 0; i < P.N; i++){
+                for (int k = 0; k < 3; k++){
+                    outfile << past[i].position[2-k] << " ";
                     if (k == 2) outfile << endl;
                 }
             }
@@ -190,25 +270,25 @@ class MD {
          */
         vecd verlet(double t, double tburn, double h){
             
-            for (unsigned n = 0; n * h < t; n++){
+            for (int n = 0; n * h < t; n++){
                 
                 if (n == 0){
-                    for (unsigned i = 0; i < P.N; i++){
-                        for (unsigned k = 0; k < 3; k++){
-                            present[i].position[k] = past[i].position[k] + h * past[i].momentum[k] / P.mass + h*h / 2.0;
+                    for (int i = 0; i < P.N; i++){
+                        for (int k = 0; k < 3; k++){
+                            present[i].position[k] = past[i].position[k] + h * past[i].velocity[k] / P.mass + h*h / 2.0;
                         }
                     }
                 }
 
                 vecd F = force();
                 // update the "future"
-                for (unsigned i = 0; i < P.N; i++){
-                    for (unsigned k = 0; k < 3; k++){
+                for (int i = 0; i < P.N; i++){
+                    for (int k = 0; k < 3; k++){
                         // next position
                         future[i].position[k] = 2*present[i].position[k] - past[i].position[k] + h*h * F[3*i + k];
                         // we have to calculate the new velocity before applying boundary conditions -> otherwise
                         // the velocity will be extremely large...
-                        future[i].momentum[k] = P.mass*(future[i].position[k] - past[i].position[k]) / (2 * h);
+                        future[i].velocity[k] = (future[i].position[k] - past[i].position[k]) / (2 * h);
                         // we need to account for periodic boundary conditions
                         if (future[i].position[k] < 0) future[i].position[k] += P.L[k];
                         else if (future[i].position[k] > P.L[k]) future[i].position[k] -= P.L[k]; 
@@ -229,9 +309,56 @@ class MD {
             return present[0].position;
         }
 
-        vecd velocity_verlet(double t, double tburn, double h){
+        void velocity_verlet(double t, double tburn, double h){
+            for (int n = 0; n * h < t; n++){
+                // just adding initial conditions
+                if (n == 0){
+                    for (int i = 0; i < P.N; i++){
+                        for (int k = 0; k < 3; k++){
+                            present[i].velocity[k] = past[i].velocity[k];
+                            present[i].position[k] = past[i].position[k];
+                            future[i].position[k] = past[i].position[k];
+                        }
+                    }
+                }
+                
+                //int c = 0;
+
+                vecd F = force(); // this has 3N dimensions
+                for (int i = 0; i < P.N; i++){
+                    for (int k = 0; k < 3; k++){
+                        present[i].velocity[k] += h/(2*P.mass) * F[3*i + k];
+                        future[i].position[k] = present[i].position[k] + h*present[i].velocity[k];
+                        if (future[i].position[k] > P.L[k]) future[i].position[k] -= P.L[k];
+                        else if (future[i].position[k] < 0) future[i].position[k] += P.L[k];
+                        // if (fabs(future[i].position[k]) > P.L[0] && c==0){
+                        //     cout << "here" << endl;
+                        //     cout << future[i].position[k] << endl; 
+                        //     c++;
+                        // }
+                    }
+                }
+                F = force();
+                for (int i = 0; i < P.N; i++){
+                    for (int k = 0; k < 3; k++){
+                        future[i].velocity[k] = present[i].velocity[k] + h/(2*P.mass) * F[3*i + k];
+                    }
+                }
+            }
+
 
         }
+
+        bool reverse(){
+            for (int i = 0; i < P.N; i++){
+                for (int k = 0; k < 3; k++){
+                    past[i].position[k] = future[i].position[k];
+                    past[i].velocity[k] = -future[i].velocity[k];
+                }
+            }
+            return true;
+        }
+
 
         /**
          * @brief function to calculate the average kinetic energy per particle
@@ -246,20 +373,63 @@ class MD {
             // calculating total energy
             for (int i = 0; i < present.size(); i++){
                 for (int j = 0; j < 3; j++){
-                    T += present[i].momentum[j] * present[i].momentum[j];
+                    T += present[i].velocity[j] * present[i].velocity[j];
                 }
                 
             }
 
-            return T / double(2 * P.mass * present.size());
+            return P.mass*T / double(2 * present.size());
         }
 
+        vecd print_past(){
+            vecd output (past.size() * 3);
+            for (int i = 0; i < present.size(); i++){
+                for (int j = 0; j < 3; j++){
+                    output[3*i + j] = past[i].position[j];
+                }
+            }
+            return output;
+        }
 
+        vecd print_present(){
+            vecd output (present.size() * 3);
+            for (int i = 0; i < present.size(); i++){
+                for (int j = 0; j < 3; j++){
+                    output[3*i + j] = present[i].position[j];
+                }
+            }
+            return output;
+        }
+        vecd print_future(){
+            vecd output (future.size() * 3);
+            for (int i = 0; i < present.size(); i++){
+                for (int j = 0; j < 3; j++){
+                    output[3*i + j] = future[i].position[j];
+                }
+            }
+            return output;
+        }
 
 
 };
 
 
+template<typename Engine>
+class ClusterWriter {
+  const MD<Engine>& md;
+public:
+  ClusterWriter(const MD<Engine>& md) : md(md) { }
+
+  friend std::ostream& operator << (std::ostream& out, const ClusterWriter& SW) {
+    const auto& sites = SW.md.past;
+    for(int i = 0; i < sites.size(); i++){
+        for (int k = 0; k < 3; k++){
+            out << sites[i].position[k] << " ";
+        }
+    } 
+    return out;
+  }
+};
 
 
 
@@ -279,7 +449,7 @@ int main(int argc, char* argv[]){
     // Output parameters
     std::string output = "";
     std::string method = ""; 
-    double burnin = 1000, until = 5000, every = 2.5;
+    double burnin = 0, until = 1, every = 0.001;
     double rho = 0.55;
 
 
@@ -299,6 +469,23 @@ int main(int argc, char* argv[]){
 
     MD md(P, rng);
 
+    vecd initial = md.print_past();
+    cout << "before" << endl;
+    md.velocity_verlet(until, burnin, every);
+    cout << "after" << endl;
+    bool work = md.reverse();
+    cout << "reverse" << endl;
+    md.velocity_verlet(until, burnin, every);
+    cout << "after reverse" << endl;
+    vecd final = md.print_present();
+
+    ofstream outfile;
+    outfile.open("position_comparison.txt");
+    for (int i = 0; i < initial.size(); i++){
+        outfile << initial[i] << " " << final[i] << endl;
+    }
+
+    if (work) cout << "nice" << endl;
 
 
     return 0;
--- proj3/test.py
+++ proj3/test.py
@@ -4,29 +4,36 @@ import matplotlib.pyplot as plt
 """Just testing some stuff 
 """
 
-mom = np.genfromtxt("test.txt", delimiter=" ")
+# mom = np.genfromtxt("test.txt", delimiter=" ")
 
-#print(mom)
+# n = 1
+# n1 = 2
 
-n = 1
-n1 = 2
+# coord = []
+# coord1 = []
 
-coord = []
-coord1 = []
+# for i in range(72*n, 72*n1):
+#     coord.append(mom[i, 0])
+#     coord.append(mom[i, 1])
+#     coord.append(mom[i, 2])
 
-for i in range(72*n, 72*n1):
-    coord.append(mom[i, 0])
-    coord.append(mom[i, 1])
-    coord.append(mom[i, 2])
+# for i in range(72*n1, 72*(n1+1)):
+#     coord1.append(mom[i, 0])
+#     coord1.append(mom[i, 1])
+#     coord1.append(mom[i, 2])
 
-for i in range(72*n1, 72*(n1+1)):
-    coord1.append(mom[i, 0])
-    coord1.append(mom[i, 1])
-    coord1.append(mom[i, 2])
+# plt.scatter(x=coord[::3], y=coord[1::3], c="red")
+# plt.scatter(x=coord1[::3], y=coord1[1::3], c="green")
 
-plt.scatter(x=coord[::3], y=coord[1::3], c="red")
-plt.scatter(x=coord1[::3], y=coord1[1::3], c="green")
+# print(coord1[3:6])
 
-print(coord1[3:6])
+# plt.show()
 
+
+
+c = np.genfromtxt("position_comparison.txt", delimiter=" ")
+
+fig = plt.figure()
+plt.hist(c[:, 0]-c[:, 1], bins=60, density=True)
+plt.axis([-1, 1, 0, 1])
 plt.show()
